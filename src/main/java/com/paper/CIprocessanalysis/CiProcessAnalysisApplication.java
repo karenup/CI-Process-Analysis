ff2260e0400ef95c53daf16f0b4d8bf3f057b873
karen <karenskw@qq.com>
2021-01-05 12:29:42 +0800

    Initial commit

---
+++ src/main/java/com/paper/CIprocessanalysis/CiProcessAnalysisApplication.java
@@ -0,0 +1,13 @@
+package com.paper.CIprocessanalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiProcessAnalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiProcessAnalysisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paper/CIprocessanalysis/CiProcessAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paper.CIprocessanalysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiProcessAnalysisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
